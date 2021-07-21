ee0206c444c7ca93e3e5c14edc0c592371ac913d
saran_nns <saran_nns@hotmail.com>
2021-07-21 12:01:27 +0200

    ci upadte

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -33,7 +29,7 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     include_package_data=True,
-    install_requires=[],
+    install_requires=["numpy"],
     zip_safe=False,
 )
 
